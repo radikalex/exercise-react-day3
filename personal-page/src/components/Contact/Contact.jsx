4e6ec6ed953850ae56712ec2884984cbffe66a4a
Alex Jimenez <alexjimenezada@gmail.com>
2022-11-23 19:42:45 +0100

    Forms with Class components

--- personal-page/src/components/Contact/Contact.jsx
+++ personal-page/src/components/Contact/Contact.jsx
@@ -1,3 +1,5 @@
+/*--------------  Contact Functional Component  ---------------*/
+
 import React, { useState, useEffect } from "react";
 import {useNavigate} from 'react-router-dom';
 import './Contact.css'
@@ -75,3 +77,104 @@ const Contact = () => {
 }
 
 export default Contact
+
+
+
+
+
+
+/*--------------  Contact Class Component  ---------------*/
+// import React, { Component } from 'react'
+// import './Contact.css'
+
+// export class Contact extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             name: "",
+//             email: "",
+//             message: "",
+//             validationMessage: "",
+//             visible: false,
+//             btnDisabled: true,
+//             submitCorrect: false
+//         }
+//         this.initialState = {
+//             name: "",
+//             email: "",
+//             message: "",
+//             validationMessage: "",
+//             visible: false,
+//             btnDisabled: true,
+//             submitCorrect: false
+//         }
+//     }
+
+//     handleInputChange = (event) => {
+//         this.setState({
+//             [event.target.name]: event.target.value,
+//         });
+//         if(event.target.name === "name") {
+//             this.setState( { 
+//                 visible: true
+//             });
+//             if (event.target.value.length < 4) {
+//                 this.setState( { 
+//                     validationMessage: "Name must be at least 4 characters",
+//                     btnDisabled: true
+//                 });
+//             } else {
+//                 this.setState( { 
+//                     validationMessage: null,
+//                     btnDisabled: false,
+//                     visible: false
+//                 });
+//             }
+//         }
+//     };
+        
+//     clearState = () => {
+//         this.setState({ ...this.initialState });
+//     };
+
+//     handleSubmit = (event) => {
+//         event.preventDefault();
+//         localStorage.setItem('formReact', JSON.stringify({
+//             name: this.state.name,
+//             email: this.state.email,
+//             message: this.state.message
+//         }))
+//         this.clearState();
+//         setTimeout(() => {
+//             console.log(this.props);
+//             this.props.history.push('/')
+//         }, 3000);
+//         this.setState( { 
+//             submitCorrect: true,
+//             visible: false
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div className='contact'>
+//                 <div className='contact-container'>
+//                     <h1>Contact</h1>
+//                     <form onSubmit={this.handleSubmit}>
+//                         <label htmlFor="name">Name:</label>
+//                         <input type="text" value={this.state.name} name="name" id="name" className="name" onChange={this.handleInputChange} />
+//                         <label htmlFor="email">Email:</label>
+//                         <input type="email" value={this.state.email} name="email" id="email" className="email" onChange={this.handleInputChange} />
+//                         <label htmlFor="message">Message:</label>
+//                         <textarea name="message" value={this.state.message} id="message" className="message" rows="3" onChange={this.handleInputChange}></textarea>
+//                         <span className="validation-error">{ this.state.visible ? this.state.validationMessage : null }</span>
+//                         <span className="validation-success">{ this.state.submitCorrect ? 'Redirecting to Home...' : null }</span>
+//                         <button type="submit" className="submit" disabled={this.state.btnDisabled}>Submit</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Contact
--- restaurant/src/components/Reserve/Reserve.jsx
+++ restaurant/src/components/Reserve/Reserve.jsx
@@ -1,3 +1,5 @@
+/*--------------  Reserve Functional Component  ---------------*/
+
 import React, { useState, useEffect } from "react";
 import {useNavigate} from 'react-router-dom';
 import './Reserve.css'
@@ -56,22 +58,123 @@ const Reserve = () => {
 
     return (
         <div className='reserve'>
-        <div className='reserve-container'>
-            <h1>Reserve</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" value={data.name} name="name" id="name" className="name" onChange={handleInputChange} />
-                <label htmlFor="date">Date:</label>
-                <input type="date" value={data.date} name="date" id="date" className="date" onChange={handleInputChange} />
-                <label htmlFor="comment">Reservation comment:</label>
-                <textarea name="comment" value={data.comment} id="comment" className="comment" rows="3" onChange={handleInputChange}></textarea>
-                <span className="validation-error">{ visible ? validationMessage : null }</span>
-                <span className="validation-success">{ submitCorrect ? 'Redirecting to Home...' : null }</span>
-                <button type="submit" className="submit" disabled={btnDisabled}>Submit</button>
-            </form>
-        </div>
+            <div className='reserve-container'>
+                <h1>Reserve</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" value={data.name} name="name" id="name" className="name" onChange={handleInputChange} />
+                    <label htmlFor="date">Date:</label>
+                    <input type="date" value={data.date} name="date" id="date" className="date" onChange={handleInputChange} />
+                    <label htmlFor="comment">Reservation comment:</label>
+                    <textarea name="comment" value={data.comment} id="comment" className="comment" rows="3" onChange={handleInputChange}></textarea>
+                    <span className="validation-error">{ visible ? validationMessage : null }</span>
+                    <span className="validation-success">{ submitCorrect ? 'Redirecting to Home...' : null }</span>
+                    <button type="submit" className="submit" disabled={btnDisabled}>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
 
 export default Reserve
+
+
+
+
+
+/*--------------  Reserve Class Component  ---------------*/
+// import React, { Component } from 'react'
+// import { useNavigate } from "react-router-dom";
+// import './Reserve.css'
+
+// export class Reserve extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             name: "",
+//             date: "",
+//             comment: "",
+//             validationMessage: "",
+//             visible: false,
+//             btnDisabled: true,
+//             submitCorrect: false
+//         }
+//         this.initialState = {
+//             name: "",
+//             date: "",
+//             comment: "",
+//             validationMessage: "",
+//             visible: false,
+//             btnDisabled: true,
+//             submitCorrect: false
+//         }
+//     }
+
+//     handleInputChange = (event) => {
+//         this.setState({
+//             [event.target.name]: event.target.value,
+//         });
+//         if(event.target.name === "name") {
+//             this.setState( { 
+//                 visible: true
+//             });
+//             if (event.target.value.length < 4) {
+//                 this.setState( { 
+//                     validationMessage: "Name must be at least 4 characters",
+//                     btnDisabled: true
+//                 });
+//             } else {
+//                 this.setState( { 
+//                     validationMessage: null,
+//                     btnDisabled: false,
+//                     visible: false
+//                 });
+//             }
+//         }
+//     };
+        
+//     clearState = () => {
+//         this.setState({ ...this.initialState });
+//     };
+
+//     handleSubmit = (event) => {
+//         event.preventDefault();
+//         localStorage.setItem('formReactRestaurant', JSON.stringify({
+//             name: this.state.name,
+//             date: this.state.date,
+//             comment: this.state.comment
+//         }))
+//         this.clearState();
+//         setTimeout(() => {
+//             const navigate = useNavigate();
+//             navigate('/')
+//         }, 3000);
+//         this.setState( { 
+//             submitCorrect: true,
+//             visible: false
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div className='reserve'>
+//                 <div className='reserve-container'>
+//                     <h1>Reserve</h1>
+//                     <form onSubmit={this.handleSubmit}>
+//                         <label htmlFor="name">Name:</label>
+//                         <input type="text" value={this.state.name} name="name" id="name" className="name" onChange={this.handleInputChange} />
+//                         <label htmlFor="date">Date:</label>
+//                         <input type="date" value={this.state.date} name="date" id="date" className="date" onChange={this.handleInputChange} />
+//                         <label htmlFor="comment">Reservation comment:</label>
+//                         <textarea name="comment" value={this.state.comment} id="comment" className="comment" rows="3" onChange={this.handleInputChange}></textarea>
+//                         <span className="validation-error">{ this.state.visible ? this.state.validationMessage : null }</span>
+//                         <span className="validation-success">{ this.state.submitCorrect ? 'Redirecting to Home...' : null }</span>
+//                         <button type="submit" className="submit" disabled={this.state.btnDisabled}>Submit</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Reserve
